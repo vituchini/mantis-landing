71e62d00e1d27b33d80b2b54a0402d055940ad04
alterproger <alterproger@gmail.com>
2023-10-19 19:00:55 +0300

    Add custom button shapes

---
+++ src/components/call-to-action-button/ButtonSmallContainedShape.tsx
@@ -0,0 +1,26 @@
+const ButtonSmallContainedShape = () => {
+  return (
+    <svg className="button-shape" width="164" height="40" viewBox="0 0 164 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M144.528 8.14963C145.468 7.25775 146 6.01916 146 4.72373V4.72373C146 2.11489 143.885 0 141.276 0H8C3.58172 0 0 3.58172 0 8V32C0 36.4183 3.58172 40 8 40H107.785C109.834 40 111.806 39.2132 113.293 37.802L144.528 8.14963Z"
+        fill="currentColor"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M123.294 32.8356C122.468 33.6197 122 34.7085 122 35.8473V35.8473C122 38.1408 123.859 40 126.153 40H152C158.627 40 164 34.6274 164 28V4.2999C164 1.92513 162.075 0 159.7 0V0C158.311 0 157.036 0.700909 156.071 1.70034C155.919 1.85738 155.763 2.01185 155.604 2.16363L123.294 32.8356Z"
+        fill="currentColor"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M152.044 20.8843L145 27.7328L146.175 29L153.219 22.1513L153.084 27.4578L154.79 27.5031L155 19.2151L146.907 19L146.862 20.7466L152.044 20.8843Z"
+        fill="white"
+      />
+    </svg>
+  );
+};
+
+export default ButtonSmallContainedShape;
