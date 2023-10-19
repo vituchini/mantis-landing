0971ac1539fca1369c26b3cfa7643e33e8ba7f64
alterproger <alterproger@gmail.com>
2023-10-19 19:06:10 +0300

    Add burger menu in header

--- src/layout/LandingLayout/Header.tsx
+++ src/layout/LandingLayout/Header.tsx
@@ -1,19 +1,49 @@
-import { Button, CardMedia, Container, Link, Stack } from '@mui/material';
+import { Box, CardMedia, Container, IconButton, Link, Menu, MenuItem, Stack, SvgIcon } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import { useTheme } from '@mui/material/styles';
 import { Link as RouterLink } from 'react-router-dom';
 
 import logo from 'assets/images/artboard-logo.svg';
+import { useState } from 'react';
+
+const LINKS = [
+  {
+    label: 'Our services',
+    path: '#'
+  },
+  {
+    label: 'How it works',
+    path: '#'
+  },
+  {
+    label: 'Benefits',
+    path: '#'
+  },
+  {
+    label: 'Use Cases',
+    path: '#'
+  }
+];
 
 const Header = () => {
   const theme = useTheme();
+
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
   return (
     <AppBar
       position="relative"
       sx={{ bgcolor: 'transparent', color: theme.palette.text.primary, boxShadow: 'none', paddingBlock: 1.5, mt: 3 }}
     >
-      <Container sx={{ display: 'flex', alignItems: 'center', gap: '89px' }}>
+      <Container sx={{ display: 'flex', alignItems: 'center', gap: '89px', justifyContent: { xs: 'space-between', md: 'start' } }}>
         <Link component={RouterLink} to="/landing" color="inherit" underline="none">
           <CardMedia component="img" image={logo} />
         </Link>
@@ -22,6 +52,7 @@ const Header = () => {
           direction="row"
           gap={5}
           sx={{
+            display: { xs: 'none', md: 'flex' },
             flex: 1,
             '& .header-link': {
               color: theme.palette.grey[600],
@@ -32,21 +63,80 @@ const Header = () => {
             }
           }}
         >
-          <Link className="header-link" href="#" underline="none">
-            Our services
-          </Link>
-          <Link className="header-link" href="#" underline="none">
-            How it works
-          </Link>
-          <Link className="header-link" href="#" underline="none">
-            Benefits
-          </Link>
-          <Link className="header-link" href="#" underline="none">
-            Use Cases
-          </Link>
+          {LINKS.map(({ label, path }) => (
+            <Link key={label} href={path} className="header-link" underline="none">
+              {label}
+            </Link>
+          ))}
         </Stack>
         {/* TODO: change button to match design */}
         <Button variant="contained">Get Started</Button>
+        <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+          >
+            <SvgIcon>
+              <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium" focusable="false" aria-hidden="true" viewBox="0 0 24 24">
+                <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
+              </svg>
+            </SvgIcon>
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left'
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left'
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{
+              display: { xs: 'block', md: 'none' },
+              maxWidth: '230px',
+              width: '100%'
+            }}
+          >
+            {LINKS.map(({ label, path }) => (
+              <MenuItem
+                key={label}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  alignItems: 'stretch'
+                }}
+              >
+                <Link
+                  sx={{
+                    display: 'flex',
+                    width: '100%',
+                    alignItems: 'center',
+                    color: theme.palette.grey[600],
+
+                    '&:hover': {
+                      color: theme.palette.primary.main
+                    }
+                  }}
+                  underline="none"
+                  href={path}
+                >
+                  {label}
+                </Link>
+              </MenuItem>
+            ))}
+            <MenuItem onClick={handleCloseNavMenu}>
+              <CallToActionButton label="Get Started" href="#" variant="contained" size="small" />
+            </MenuItem>
+          </Menu>
+        </Box>
       </Container>
     </AppBar>
   );
