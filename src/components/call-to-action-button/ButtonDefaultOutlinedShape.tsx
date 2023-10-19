c80e6d184d0e4702124247ff55d01cbcfa53a73d
alterproger <alterproger@gmail.com>
2023-10-19 19:26:03 +0300

    Fix props names

--- src/components/call-to-action-button/ButtonDefaultOutlinedShape.tsx
+++ src/components/call-to-action-button/ButtonDefaultOutlinedShape.tsx
@@ -3,8 +3,8 @@ const ButtonDefaultOutlinedShape = () => {
     <svg className="button-shape" width="259" height="56" viewBox="0 0 259 56" fill="none" xmlns="http://www.w3.org/2000/svg">
       <mask id="path-1-inside-1_413_123" fill="white">
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M234.311 9.35655C235.389 8.33258 236 6.91057 236 5.4233V5.4233C236 2.42809 233.572 0 230.577 0H12C5.37258 0 0 5.37258 0 12V44C0 50.6274 5.37257 56 12 56H181.711C184.234 56 186.537 54.7081 188.367 52.9708V52.9708L234.311 9.35655Z"
         />
       </mask>
@@ -15,8 +15,8 @@ const ButtonDefaultOutlinedShape = () => {
       />
       <mask id="path-3-inside-2_413_123" fill="white">
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M201.689 46.6435C200.611 47.6675 200 49.0895 200 50.5767V50.5767C200 53.5719 202.428 56 205.423 56H247C253.627 56 259 50.6274 259 44V5.76378C259 2.58053 256.419 0 253.236 0V0C251.374 0 249.664 0.937614 248.378 2.28536C248.137 2.53806 247.889 2.78611 247.633 3.0292L201.689 46.6435Z"
         />
       </mask>
@@ -26,8 +26,8 @@ const ButtonDefaultOutlinedShape = () => {
         mask="url(#path-3-inside-2_413_123)"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M241.862 30.638L232 40.226L233.645 42L243.506 32.4119L243.318 39.8409L245.706 39.9044L246 28.3011L234.669 28L234.607 30.4452L241.862 30.638Z"
         fill="currentColor"
       />
