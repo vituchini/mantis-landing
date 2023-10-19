7ad1e9e305e51073957ce9ea075268de68373fab
alterproger <alterproger@gmail.com>
2023-10-19 16:43:48 +0300

    Refactor styled title text components

--- src/sections/landing/SectionTitle.tsx
+++ src/sections/landing/SectionTitle.tsx
@@ -1,41 +1,23 @@
-import { Typography, useTheme } from '@mui/material';
+import { Typography, styled, type TypographyProps, type TypographyTypeMap } from '@mui/material';
+import { type OverridableComponent } from '@mui/material/OverridableComponent';
 
-const SectionTitle = ({ children }: React.PropsWithChildren) => {
-  const theme = useTheme();
+const SectionTitle = styled<OverridableComponent<TypographyTypeMap<{}, 'span'>>>((props: TypographyProps) => (
+  <Typography variant="h2" {...props} />
+))(({ theme }) => ({
+  fontSize: 64,
+  fontWeight: 300,
+  letterSpacing: -1,
+  color: theme.palette.grey.A400
+}));
 
-  return (
-    <Typography
-      variant="h2"
-      sx={{
-        fontSize: 64,
-        fontWeight: 300,
-        letterSpacing: -1,
-        color: theme.palette.grey.A400
-      }}
-    >
-      {children}
-    </Typography>
-  );
-};
+const HighlightedTitleText = styled<OverridableComponent<TypographyTypeMap<{}, 'span'>>>((props: TypographyProps) => (
+  <Typography variant="h2" component="span" {...props} />
+))(({ theme }) => ({
+  fontSize: 64,
+  fontWeight: 700,
+  fontStyle: 'italic',
+  letterSpacing: -1,
+  color: theme.palette.text.primary
+}));
 
-const HighlightedTitleText = ({ children }: React.PropsWithChildren) => {
-  const theme = useTheme();
-
-  return (
-    <Typography
-      variant="h2"
-      component="span"
-      sx={{
-        fontSize: 64,
-        fontWeight: 700,
-        fontStyle: 'italic',
-        letterSpacing: -1,
-        color: theme.palette.text.primary
-      }}
-    >
-      {children}
-    </Typography>
-  );
-};
-
-export { SectionTitle, HighlightedTitleText };
+export { HighlightedTitleText, SectionTitle };
